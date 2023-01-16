b8620cc8088856264d5679fa2e0a47506402d81f
mosmohamed1 <mostafa.mohamed1@web.de>
2023-01-16 01:22:03 +0100

    contact + footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './contact.css'
+import {MdOutlineMail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_tcc2n6e', 'template_rl98f2o', form.current, 'l0Ua2LfN05mB3v3xw')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+  };
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+    <h5>Get in Touch</h5>
+    <h2>Contact Me</h2>
+    <div className="container contact-container">
+      <div className="contact-options">
+        <article className="contact-option">
+          <MdOutlineMail className='icon'/>
+          <h4>Email</h4>
+          <h5>morezk250@gmail.com</h5>
+          <a href="mailto:morezk250@gmail.com" target='_blank'>Send a Message</a>
+        </article>
+        
+        <article className="contact-option">
+          <RiMessengerLine className='icon'/>
+          <h4>Massenger</h4>
+          <h5>Mostafa Rezk</h5>
+          <a href="https://m.me/mostafamrezk1/" target='_blank'>Send a Message</a>
+        </article>
+        
+        
+        <article className="contact-option">
+          <BsWhatsapp className='icon' />
+          <h4>WhatsApp</h4>
+          <h5>+4917658863018</h5>
+          <a href="https://api.whatsapp.com/send?phone=+4917658863018" target='_blank'>Send a Message</a>
+        </article>
+        </div>
+      <form ref={form} onSubmit={sendEmail} >
+        <input type="text" name='name' placeholder='Your Name' required />
+        <input type="email" name='email' placeholder='Your Email' required />
+        <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+        <button type='submit' className='btn btn-primary'>Submit</button>
+      </form>
+      </div>
+    </section>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import './footer.css'
-
+import {FaFacebookF,FaInstagram,FaTwitter} from 'react-icons/fa'
+import logo from '../../assets/Mostafa Rezk-logos_white.png'
 export const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer id='footer'>
+      <a href="#header"><img className='logo' src={logo} alt="logo" /></a>
+      <ul className="permalinks">
+      <li><a href="#header">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#services">Servicces</a></li>
+      <li><a href="#portoflio">Portoflio</a></li>
+      <li><a href="#testimonials">Testimonials</a></li>
+      <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer-social">
+        <a href="https://www.facebook.com/mostafamrezk1/">
+          <FaFacebookF />
+        </a>
+        <a href="https://www.instgram.com/mostafamrezk1/">
+        <FaInstagram />
+        </a>
+        <a href="https://www.twitter.com/mostafamrezk1/">
+          <FaTwitter />
+        </a>
+
+      </div>
+      <div className="footer-copyright">
+        <small>&copy; Mostafa Rezk .All rights reserved </small>
+      </div>
+    </footer>
   )
 }
